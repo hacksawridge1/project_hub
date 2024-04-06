2a6b94c0632128e77dcb7abb12334f5724affe22
ivan borodkin <borodkin.ivan2003@gmail.com>
2024-04-06 23:36:40 +0300

    minor updts

--- src/server/main.py
+++ src/server/main.py
@@ -1,25 +1,26 @@
 import socket
-from modules.objects import decrypt_data
+# from modules.objects import decrypt_data
 import json
-from modules.app import App
+# from modules.app import App
 
 def main():
-  app = App()
-  sock = socket.socket()
+  # app = App()
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   sock.bind(('', 9090))
   sock.listen(1)
-  conn, addr = sock.accept()
 
+  # sock.close()
 
-  print('connect:', addr)
+  # print('connect:', addr)
 
-  while True:
-    data = conn.recv(1024)
-    if not data:
-      break
-    if data == 'close conn':
-      conn.close()
-    conn.send(decrypt_data(data, App.private_key))
+  # while True:
+  #   conn, addr = sock.accept()
+  #   data = conn.recv(1024)
+  #   if not data:
+  #     break
+  #   if data == 'close conn':
+  #     conn.close()
+  #   conn.send(decrypt_data(data, App.private_key))
 
 if __name__ == '__main__':
   main()
