503111e99b569f2e614a2222e8063115d4a0d83e
Maxim <maxim2005ua@gmail.com>
2024-04-07 16:48:17 +0300

    JSON в работе

--- app/model/json/settings.py
+++ app/model/json/settings.py
@@ -1,5 +1,7 @@
 import json
 
+# Нужно детализировать
+
 class Json():
     data = {}
     def __init__(self):
@@ -18,28 +20,30 @@ class Json():
     # Создать JSON
     def create_json(self, data):
         with open('settings.json', 'w') as file:
-            json.dump(data, file)
+            # Валидация data
+            json.dump(self.data, file)
     
     # Редактирование JSON
     def edit_json(self, new_data):
-        existing_data = x.read_json(self)
+        existing_data = self.read_json(self)
         existing_data.update(new_data)
         with open('settings.json', 'w') as file:
             json.dump(existing_data, file)
     
     # Удаление JSON
-    def delete_json_item(self, key):
-        existing_data = x.read_json(self)
-        if key in existing_data:
-            del existing_data[key]
-            with open('settings.json', 'w') as file:
-                json.dump(existing_data, file)
-        else:
-            print(f"Key '{key}' not found in the JSON data.")    
+    def delete_json_item(self):
+        existing_data = self.read_json(self)
+        existing_data.update(self.data)
+        with open('settings.json', 'w') as file:
+            json.dump(existing_data, file)
 
-#x = Json()
-#y = x.read_json()
-#print(y)
-#x.create_json(y)
-#x.edit_json(y)
-#x.delete_json_item(y)
+# Для локального тестирования (удалить)
+x = Json()
+y = x.read_json()
+print(y)
+x.create_json({"maxim", "avatar", "2024"})
+print(y)
+x.edit_json({"maxim2", "avatar2", "2025"})
+print(y)
+x.delete_json_item(y)
+print(y)
