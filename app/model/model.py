928baf623581abadf82c186b0c15d929d6378cb0
Maxim <maxim2005ua@gmail.com>
2024-04-07 19:09:39 +0300

    Добавил сокет в клиент

--- app/model/model.py
+++ app/model/model.py
@@ -2,10 +2,12 @@ __author__ = "Maxim"
 
 from model.json.settings import Json
 from model.sql.dataBase import Sqlite
+from model.socket.socket import Socket
 
 class Model():
     def __init__(self):
         self.json = Json()
         self.sql = Sqlite()
+        #self.socket = Socket() # Исправить потоки
         self.name = "undefined"
         self.chat = ["temp"]
