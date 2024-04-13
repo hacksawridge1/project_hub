90383194fec30432fc1b5beff68e31d37111bc53
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2024-04-13 12:52:10 +0300

    wip

--- src/client/main.py
+++ src/client/main.py
@@ -1,17 +1,25 @@
 import socket
 import json
+import requests
 
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_sock.connect(('localhost', 9090))
-with open('../objects/personal/user_info.json', 'r') as f:
-  data = f.read()
-  data = json.loads(data)
-  data = json.dumps(data)
-  client_sock.send(data.encode())
+# TEEEEEEEEEEESTS
 
-rcv_data = client_sock.recv(1024)
 
-with open('usersonline.json', 'w') as f:
-  rcv_data = rcv_data.decode()
-  rcv_data = json.loads(rcv_data)
-  f.write(json.dumps(rcv_data))
+rcv = requests.get("http://localhost:9090/usersonline")
+rcv = rcv.text
+print(rcv)
+
+# client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_sock.connect(('localhost', 9090))
+# with open('../objects/personal/user_info.json', 'r') as f:
+#   data = f.read()
+#   data = json.loads(data)
+#   data = json.dumps(data)
+#   client_sock.send(data.encode())
+
+# rcv_data = client_sock.recv(1024)
+
+# with open('usersonline.json', 'w') as f:
+#   rcv_data = rcv_data.decode()
+#   rcv_data = json.loads(rcv_data)
+#   f.write(json.dumps(rcv_data))
--- src/server/main.py
+++ src/server/main.py
@@ -1,27 +1,23 @@
 import socket
 import json
 
+# TEEEEEEEEESTS
+
 def start_server(addr: str, port: int):
   server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   server_sock.bind((addr, port))
-  server_sock.listen(10)
+  server_sock.listen()
 
   while True:
     client_sock, client_addr = server_sock.accept()
-    data = client_sock.recv(1024)
-    data = data.decode()
-    if type(json.loads(data)) is dict:
-        data = json.loads(data)
-        try:
-          with open('../objects/general/usersonline.json', 'r') as f:
-              file_data = f.read()
-              file_data = json.loads(file_data)
-              file_data['usersonline'].append(data)
-              file_data = json.dumps(file_data)
-              client_sock.send(file_data.encode())
-              client_sock.close()
-        except:
-           client_sock.close()
+    data = client_sock.recv(1024).decode('utf-8')
+    HEADERS = 'HTTP/1.1 200 OK\r\n\Content-Type: application/json; charset=utf-8\r\n\r\n'
+    if data.split(' ')[1] == '/usersonline':
+      with open('../objects/general/usersonline.json', 'r') as f:
+        ans = json.load(f)
+        ans = json.dumps(ans)
+        client_sock.send(HEADERS.encode() + ans.encode())
+        client_sock.close()
 
 start_server('', 9090)
