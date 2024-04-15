8e6c74f48328654dda977e1b6bc090d368615f35
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2024-04-16 00:24:23 +0300

    minor updates

--- src/server/main.py
+++ src/server/main.py
@@ -14,18 +14,19 @@ def start_server(addr: str, port: int):
     client_sock, client_addr = server_sock.accept()
     data = client_sock.recv(1024).decode()
     print(data)
-    # server_sock.close()
-    # method = data.split(' ')[0]
-    # request = data.split(' ')[1]
-    # if method == "GET":
-    #   HEADERS = 'HTTP/1.1 200 OK\r\n\Content-Type: application/json; charset=utf-8\r\n\r\n'
-    #   if request == '/usersonline':
-    #     with open('../objects/general/usersonline.json', 'r') as f:
-    #       ans = encrypt_object(json.load(f))
-    #       ans = json.dumps(ans)
-    #       client_sock.send(HEADERS.encode() + ans.encode())
-    #       client_sock.close()
-    #   if request == 'user_info':
-    #     pass
-    
+    method = data.split(' ')[0]
+    request = data.split(' ')[1]
+    if method == "GET":
+      HEADERS = 'HTTP/1.1 200 OK\r\n\Content-Type: application/json; charset=utf-8\r\n\r\n'
+      # if request == '/usersonline':
+      #   with open('../objects/general/usersonline.json', 'r') as f:
+      #     ans = encrypt_object(json.load(f))
+      #     ans = json.dumps(ans)
+      #     client_sock.send(HEADERS.encode() + ans.encode())
+      #     client_sock.close()
+      # if request == 'user_info':
+      #   pass
+      if request == '/hi':
+        client_sock.send(HEADERS.encode()+"Hi".encode())
+
 start_server('', 9091)
