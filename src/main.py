0e4d6d6cfc2ca0dce3ba2ee534864d971530e739
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2024-04-21 02:13:15 +0300

    wip

--- src/main.py
+++ src/main.py
@@ -9,33 +9,28 @@ from multiprocessing import freeze_support, Process
 from _thread import start_new_thread
 from pprint import pprint
 
-user = User('ivan')
-data = input()
-encrypted_data = encrypt_data(data, user.public_key)
-print(decrypt_data(encrypted_data, user.private_key))
+user_name = input("Введите ваше имя:\t")
+# user_passphrase = input("Введите кодовое слово для защиты ваших ключей:\t")
+print('Генерируем данные...')
+app = App()
+user = User(user_name)
+# server = Server(user)
+server_thread = Thread(target=start_server, args=(user, ))
+server_thread.start()
+init_thread = Thread(target=user.initial)
+init_thread.start()
+print("Генерация прошла успешно")
 
-# user_name = input("Введите ваше имя:\t")
-# # user_passphrase = input("Введите кодовое слово для защиты ваших ключей:\t")
-# print('Генерируем данные...')
-# app = App()
-# user = User(user_name)
-# # server = Server(user)
-# server_thread = Thread(target=start_server, args=(user, ))
-# server_thread.start()
-# init_thread = Thread(target=user.initial)
-# init_thread.start()
-# print("Генерация прошла успешно")
+reciever_name = input('Введите имя получателя:\t')
+reciever_ip = input('Введите айпи получателя:\t')
 
-# reciever_name = input('Введите имя получателя:\t')
-# reciever_ip = input('Введите айпи получателя:\t')
-
-# while True:
-#   data = input('Введите сообщение:\t')
-#   if data == 'stop':
-#     break  
-#   elif data == 'Пользователи':
-#     print(user.users_online)
-#   else:
-#     user.send_message(reciever_ip, reciever_name, data)
+while True:
+  data = input('Введите сообщение:\t')
+  if data == 'stop':
+    break  
+  elif data == 'Пользователи':
+    print(user.users_online)
+  else:
+    user.send_message(reciever_ip, reciever_name, data)
 # # server_thr = threading.Thread(target=start_server, args=('', 9091, user), name='server')
 # # server_thr.start()
