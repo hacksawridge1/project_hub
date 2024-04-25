d28e06ff1a9b097b1444f06747209ee217c19928
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2024-04-26 00:35:45 +0300

    wip

--- src/main.py
+++ src/main.py
@@ -1,33 +1,29 @@
 from modules.user import User
 from modules.app import App
 from modules.new_server import start_server
-# from _thread import start_new_thread
 from threading import Thread
 import sys
 from time import sleep
 from modules.objects import encrypt_object
 
-# def check_server():
-#   while True:
-#     print(server_thread.is_alive())
-#     sleep(3)
-
 user_name = input("Введите ваше имя:\t")
 # user_passphrase = input("Введите кодовое слово для защиты ваших ключей:\t")
 print('Генерируем данные...')
+
 app = App()
 user = User(user_name)
-# server = Server(user)
+
 server_thread = Thread(target=start_server, args=(user, ))
 server_thread.start()
-# check_server_thread = Thread(target=check_server)
-# check_server_thread.start()
 init_thread = Thread(target=user.initial)
 init_thread.start()
+
 print("Генерация прошла успешно")
 print(user.users_online)
+
 reciever_ip = input('Введите ip пользователя:\t')
 reciever_name = input('Введите имя пользователя:\t')
+
 while True:
   data = input('Введите сообщение:\t')
   if data == 'stop':
