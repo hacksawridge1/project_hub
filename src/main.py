5e556f9df1b10e6c71db9e3546c041737cdeeac2
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2024-05-06 17:38:15 +0300

    wip

--- src/main.py
+++ src/main.py
@@ -7,50 +7,29 @@ from time import sleep
 from modules.objects import encrypt_object, decrypt_object, find_in_object
 import json
 
-#user_name = input("Введите ваше имя:\t")
-#user_passphrase = input("Введите кодовое слово для защиты ваших ключей:\t")
-#print('Генерируем данные...')
+user_name = input("Введите ваше имя:\t")
+print('Генерируем данные...')
 
-#app = App()
-#user = User(user_name)
+app = App()
+user = User(user_name)
 
-user = {
-  'user_name': 'Ivan',
-  'user_ip': '192.168.3.121'
-}
+server_thread = Thread(target=start_server, args=(user, ))
+server_thread.start()
+init_thread = Thread(target=user.initial)
+init_thread.start()
 
-data = {
-  'users_online': [
-    {
-      'user_name': 'Ivan',
-      'user_ip': '192.168.3.121'
-    }
-  ]
-}
+print("Генерация прошла успешно")
+print(user.users_online)
 
-print(find_in_object(data, user))
+reciever_ip = input('Введите ip пользователя:\t')
+reciever_name = input('Введите имя пользователя:\t')
 
-#with open('objects/self/user-info.json', 'r') as f:
- #   print(decrypt_object(json.load(f), user.private_key))
-  #  f.close()
-
-#server_thread = Thread(target=start_server, args=(user, ))
-#server_thread.start()
-#init_thread = Thread(target=user.initial)
-#init_thread.start()
-
-#print("Генерация прошла успешно")
-#print(user.users_online)
-#
-#reciever_ip = input('Введите ip пользователя:\t')
-#reciever_name = input('Введите имя пользователя:\t')
-#
-#while True:
-#  data = input('Введите сообщение:\t')
-#  if data == 'stop':
-#    user.call_to_remove_user()
-#    sys.exit()
-#  elif data == 'Пользователи':
-#    print(user.users_online)
-#  else:
-#    user.send_message(reciever_ip, reciever_name, data)
+while True:
+  data = input('Введите сообщение:\t')
+  if data == 'stop':
+    user.call_to_remove_user()
+    sys.exit()
+  elif data == 'Пользователи':
+    print(user.users_online)
+  else:
+    user.send_message(reciever_ip, reciever_name, data)
