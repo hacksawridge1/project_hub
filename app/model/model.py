2bb38113b685d3b270edea853f1610071d7882bd
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2024-05-12 22:31:14 +0300

    wip

--- app/model/model.py
+++ app/model/model.py
@@ -8,8 +8,6 @@ class Model():
         self.json = Json()
         self.sql = Sqlite() # Загатовка для сохранения чата и т.д.
         self.listUsers = [] # Заполняется с JSON
-        self.listMessages = [] # Заполняется с JSON
-        #self.name = "undefined" # Имя текущего клиента
         self.name = self.json.readJSON()['name']
 
     ## ПОЛЬЗОВАТЕЛИ
