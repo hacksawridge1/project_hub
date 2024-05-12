38de582a1aaae3fd49740869a3da6ee8a5491fbf
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2024-05-12 22:29:11 +0300

    wip

--- src/modules/user.py
+++ src/modules/user.py
@@ -60,33 +60,6 @@ class User:
            if not address_object.is_loopback:
              return address_info['addr']
 
-#  def __recv_message(self, data: dict):
-#    print(f'From:{data["user_name"]}\nMessage:{data["message"]}')
-#    chat: dict = CHAT
-#    chat_object: dict = CHAT_OBJECT
-#    chat_object["user_name"] = data["user_name"]
-#    chat_object["user_ip"] = data["user_ip"]
-#    chat_object["time"] = f"{localtime().tm_hour}:{localtime().tm_min}" 
-#    chat_object["message"] = data["message"]
-#    chat["chat"].append(chat_object)
-#    
-#    if not os.path.exists(os.getcwd() + f'/objects/chat/{chat_object["user_name"]}_{chat_object["user_ip"]}'):
-#      os.makedirs(os.getcwd() + f'/objects/chat/{chat_object["user_name"]}_{chat_object["user_ip"]}')
-#
-#      with open(os.getcwd() + f'/objects/chat/{chat_object["user_name"]}_{chat_object["user_ip"]}/chat.json', 'w') as f:
-#        f.write(json.dumps(encrypt_object(chat, self.public_key), sort_keys=True))
-#        f.close()
-#    else:
-#
-#      with open(f'objects/chat/{chat_object["user_name"]}_{chat_object["user_ip"]}/chat.json', 'r') as f:
-#        chat = json.load(f) 
-#        f.close()
-#
-#      with open(f'objects/chat/{chat_object["user_name"]}_{chat_object["user_ip"]}/chat.json', 'w') as f:
-#        f.write(json.dumps(encrypt_object(chat, self.public_key), sort_keys=True))
-#        f.close()
-    
-
   def __initial(self) -> bool:
     net_ip: str = '.'.join(self.ip.split('.')[:3]) + '.'
     i: int = 2
@@ -124,39 +97,6 @@ class User:
       return True
     except:
       return False
-
-#  def __remove_user(self, removed_user: dict) -> Union[str, None]:
-#    file_data: dict
-#
-#    with open('objects/self/users-online.json', 'r') as f:
-#      file_data = decrypt_object(json.load(f), self.private_key)
-#      f.close()
-#
-#    if find_in_object(file_data, removed_user) != None:
-#      with open('objects/self/users-online.json', 'w') as f:
-#        f.write(json.dumps(encrypt_object(file_data['users_online'].remove(removed_user), self.public_key)))
-#        f.close()
-#    else:
-#      return 'Пользователь не найден...'
-
-#  def __add_user(self, data: dict) -> Union[str, None]:
-#    file_data: dict  
-#
-#    if data["user_pub_key"]:
-#      data.pop("user_pub_key")
-#
-#    with open('objects/self/users-online.json', 'r') as f:
-#      file_data = decrypt_object(json.load(f), self.private_key)
-#      f.close()
-#
-#    if find_in_object(file_data, data) == None:
-#      file_data['users_online'].append(data)
-#
-#      with open('objects/self/users-online.json', 'w') as f:
-#        f.write(json.dumps(encrypt_object(file_data, self.public_key)))
-#        f.close()
-#    else:
-#      return 'Пользователь уже существует'
 
   # Send message (user.send_message(...))
   def send_message(self, reciever_ip: str, reciever_name: str, message: str) -> None:
@@ -207,8 +147,8 @@ class User:
         os.remove('objects/chat/*')
         os.remove('objects/upload/*')
         os.remove('objects/download/*')
-    except:
-      print('Conn Error')
+    except Exception:
+      print(f'Conn Error: {Exception}')
       self.call_to_remove_user()
 
   # Get chat inform with {user_name, user_ip}(user.chat_info(...))
@@ -261,43 +201,8 @@ class User:
   def private_key(self):
     return self.__private_key.decode()
 
-  @property
-  def remove_user(self):
-    pass
-
-  # Remove user on leave (user.remove_user = user: User)
-  @remove_user.setter
-  def remove_user(self, data: dict):
-    return self.__remove_user(data)
-
-  @property
-  def add_user(self):
-    pass
-  
-  # Add user (user.add_user = user: User)
-  @add_user.setter
-  def add_user(self, data: dict):
-    self.__add_user(data)
-
   # Initial on setup
   @property
   def initial(self):
     self.__initial()
-
-  @property
-  def recv_message(self):
-    pass 
-
-  # Recieve message (user.recv_message = messageObject)
-  @recv_message.setter
-  def recv_message(self, data: dict):
-    return self.__recv_message(data)
-
-  #@property
-  #def chat_info(self):
-  #  pass 
-
-  #@chat_info.setter
-  #def chat_info(self, user_name: str, user_ip: str):
-  #  return self.__chat_info(user_name, user_ip)
 
