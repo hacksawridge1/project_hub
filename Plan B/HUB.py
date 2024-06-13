d080ddea0d7ea410a788f18e6d58571fc814ff4e
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2024-06-13 22:22:40 +0300

    Remove PlanB and web

--- Plan B/HUB.py
+++
@@ -1,8 +0,0 @@
-from model.model import Model
-from view.view import View
-from control.control import Control
-
-if __name__ == "__main__":
-    model = Model()
-    view = View()
-    control = Control(model, view)
--- Plan B/view/view.py
+++
@@ -1,7 +0,0 @@
-from os import path
-
-class View():
-    authorization = path.dirname(path.abspath(__file__)) + "/Authorization.qml"
-    mainWindow = path.dirname(path.abspath(__file__)) + "/MainWindow.qml"
-    sidebar = path.dirname(path.abspath(__file__)) + "/SideBar.qml"
-    chat = path.dirname(path.abspath(__file__)) + "/Chat.qml"
